96e6ecd00c7e6b500f46d3b97321190ca27bbb98
blucky8649 <blucky4511@gmail.com>
2022-02-28 18:05:06 +0900

    feat: api 네트워크 통신을 위한 retrofit2 셋업

---
+++ app/src/main/java/com/example/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "359ed88f5a914737968242ba24e9c3e9"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
