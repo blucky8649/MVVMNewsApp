c0048f8cd5494f9d87d5a7970d91a83cfed29b09
blucky8649 <blucky4511@gmail.com>
2022-02-28 23:19:25 +0900

    feat: 뷰 모델 셋업

--- app/src/main/java/com/example/mvvmnewsapp/ui/fragments/ArticleNewsFragment.kt
+++ app/src/main/java/com/example/mvvmnewsapp/ui/fragments/ArticleNewsFragment.kt
@@ -1,7 +1,40 @@
 package com.example.mvvmnewsapp.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.mvvmnewsapp.R
+import com.example.mvvmnewsapp.databinding.FragmentArticleBinding
+import com.example.mvvmnewsapp.ui.NewsActivity
+import com.example.mvvmnewsapp.ui.NewsViewModel
 
 class ArticleNewsFragment : Fragment(R.layout.fragment_article) {
+    private var _binding: FragmentArticleBinding? = null
+    private val binding get() = _binding!!
+
+    lateinit var viewModel: NewsViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as NewsActivity).viewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentArticleBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    /**
+     * Fragment에서 View Binding을 사용할 경우 Fragment는 View보다 오래 지속되어,
+     * Fregment의 Lifecycle로 인해 메모리 누수가 발생할 수 있다.
+     * 따라서 반드시 binding 변수를 onDestroyView() 이후에 null로 만들어 주어야한다.
+     */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/example/mvvmnewsapp/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/example/mvvmnewsapp/ui/fragments/SearchNewsFragment.kt
@@ -1,7 +1,41 @@
 package com.example.mvvmnewsapp.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.mvvmnewsapp.R
+import com.example.mvvmnewsapp.databinding.FragmentSavedNewsBinding
+import com.example.mvvmnewsapp.databinding.FragmentSearchNewsBinding
+import com.example.mvvmnewsapp.ui.NewsActivity
+import com.example.mvvmnewsapp.ui.NewsViewModel
 
 class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
+    private var _binding: FragmentSearchNewsBinding? = null
+    private val binding get() = _binding!!
+
+    lateinit var viewModel: NewsViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as NewsActivity).viewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSearchNewsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    /**
+     * Fragment에서 View Binding을 사용할 경우 Fragment는 View보다 오래 지속되어,
+     * Fregment의 Lifecycle로 인해 메모리 누수가 발생할 수 있다.
+     * 따라서 반드시 binding 변수를 onDestroyView() 이후에 null로 만들어 주어야한다.
+     */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
